cc97df7faa6fc93fd99eb903cd4681fc9f961e7e
tdubs42 <contact-tdubs@protonmail.com>
2021-07-21 09:59:20 -0700

    index.css reset set, favicons generated, and Suspense in place for lazy loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react'; // lazy and Suspense are used to dynamically import components for better web performance
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+
+// I will refactor imports into lazy imports after Unit 2 is done with building out their forms - tdubs
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<h2>Loading...</h2>}>
+      {/*  Unit 2 - Your code goes here :) */}
+        <Switch>
+        {/*  Unit 2 - Your code goes here :) */}
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
